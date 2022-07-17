ebdabcc47d66ca2c9c62ea22b7aafae24d3439d7
Sriyansh Kumar <74192841+SRI2914@users.noreply.github.com>
2022-07-17 19:59:46 +0530

    Add files via upload

---
+++ GUVI/script.js
@@ -0,0 +1,28 @@
+let employes=[
+    {
+        id:101,
+        name:"lavish",
+        Designation:"Sr.FSD"
+    },
+    {
+        id:102,
+        name:"sri",
+        Designation:"sir"
+    },
+    {
+        id:103,
+        name:"mona",
+        Designation:"ba"
+    },
+    {
+        id:104,
+        name:"attu",
+        Designation:"Sr.ba"
+    },
+
+];
+let employeename=[];
+employees.forEach((employee) =>m {
+    employeename.push(employee.name);
+});
+console.table(employeename);
